25357257c9486a7d12c6e345592c55e27d65d9a5
Tejaswini Simha <tejaswini_y@trigent.com>
2025-06-26 21:55:15 +0530

    Removed all tests other thant the tests session_handling ones

--- cypress/e2e/dbTesting/data_deletion_tests.cy.js
+++
@@ -1,9 +0,0 @@
-describe('Database Deletion Tests', () => {
-    it('should delete a product from the catalog', () => {
-      cy.task('queryDb', "DELETE FROM products WHERE product_id = 1")
-        .then((result) => {
-          expect(result.affectedRows).to.eq(1); // Assert that one product was deleted
-        });
-    });
-
-  });
